6ff80b15294296c6f7ea5e36bb2fa2b100b0fdf0
MD. Mustak Tahmid <md.mustak.tahmid08@gmail.com>
2024-09-15 22:26:52 +0600

    Add files via upload

---
+++ Kotlin 2nd Project/src/Main.kt
@@ -0,0 +1,34 @@
+fun main(){
+
+    var person = 1
+    var Name = "Elon Musk"
+    var Age = 53
+    val pation = "businessman"
+    var like = "Thinking"
+    println("$person. Hi $Name. You are $Age years old. And you are a $pation. You like to $like")
+
+    person = 2
+    Name = "Bill Gates"
+    Age = 68
+    like = "Making"
+    println("$person. Hi $Name. You are $Age years old. And you are a $pation. You like to $like")
+
+    person = 3
+    Name = "D. Muhammad Yunus"
+    Age = 84
+    like = "Reading"
+    println("$person. Hi $Name. You are $Age years old. And you are a $pation. You like to $like")
+
+    person = 4
+    Name = "Sundar Pichai"
+    Age = 52
+    like = "Creating"
+    println("$person. Hi $Name. You are $Age years old. And you are a $pation. You like to $like")
+
+    person = 5
+    Name = "Mark Zuckerberg"
+    Age = 40
+    like = "Coding"
+    println("$person. Hi $Name. You are $Age years old. And you are a $pation. You like to $like")
+}
+
